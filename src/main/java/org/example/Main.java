a74705fdb619e53a079f34e6245693e7c472fc47
Chandan Kumar <chandan031999@gmail.com>
2024-09-07 10:45:51 +0530

    new concept added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Welcome to Jenkins!");
         System.out.println("Welcome to Jenkins!");
         System.out.println("Welcome to Jenkins!");
+        System.out.println("Welcome to Jenkins!");
 
     }
 }
